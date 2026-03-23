a044ce3d7e6f1d05e11544c3244109945981c9c9
Henrique Schorck <henrique_schorck@estudante.sesisenai.org.br>
2026-03-23 08:42:31 -0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,42 +15,51 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 load_dotenv() #load .env file, where email and password are stored
+py.mouseInfo
 
-def open_and_log_in():
-    chrome_options = Options()
+chrome_options = Options()
 
-    # Open in Incognito & Dark Mode
-    chrome_options.add_argument("--incognito")
-    chrome_options.add_argument("--force-dark-mode")
-    chrome_options.add_argument("--enable-features=WebUIDarkMode")
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_experimental_option("detach", True)
+# Open in Incognito & Dark Mode
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--force-dark-mode")
+chrome_options.add_argument("--enable-features=WebUIDarkMode")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_experimental_option("detach", True)
 
-    # Hide basic automation banners
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+# Hide basic automation banners
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
-    servicce = Service(ChromeDriverManager().install())
-    browser = webdriver.Chrome(service=servicce, options=chrome_options)
+servicce = Service(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=servicce, options=chrome_options)
 
-    email = os.getenv("email")
-    password = os.getenv("password")
+email = os.getenv("email")
+password = os.getenv("password")
     
-    # logging into google
+# logging into google
    
-    browser.get("https://accounts.google.com/")
+browser.get("https://accounts.google.com/")
 
-    email_input = WebDriverWait(browser, 30).until(
-        EC.visibility_of_element_located((By.ID, "identifierId"))
-    )
-    email_input.send_keys(email + Keys.ENTER)
+email_input = WebDriverWait(browser, 30).until(
+    EC.visibility_of_element_located((By.ID, "identifierId"))
+)
+email_input.send_keys(email + Keys.ENTER)
 
-    password_input = WebDriverWait(browser, 30).until(
-        EC.visibility_of_element_located((By.XPATH, "//input[@type='password']"))
-    )
-    time.sleep(1)
-    password_input.send_keys(password + Keys.ENTER)
-    time.sleep(5)
+password_input = WebDriverWait(browser, 30).until(
+    EC.visibility_of_element_located((By.XPATH, "//input[@type='password']"))
+)
+time.sleep(1)
+password_input.send_keys(password + Keys.ENTER)
+time.sleep(5)
     
-    browser.get("https://www.gmail.com")
+browser.get("https://forms.gle/SGy2pnLA9LdQfXeSA") #opens the form's link (test)
+
+# opening windows explorer and finding + opening the file
+py.hotkey('win', 'e')
+time.sleep(3)
+py.hotkey('ctrl', 'f')
+time.sleep(3)
+py.write("Base_dados.xlsx")
+
+
